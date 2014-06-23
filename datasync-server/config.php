051ef5e8d0e7da7030b0a837d22e0afa65392aa1
kris <kris.randall@gmail.com>
2014-06-23 12:28:24 +1000

    config.php as a template, rather than as an example

--- datasync-server/config.php
+++ datasync-server/config.php
@@ -3,27 +3,27 @@
 
 // ** App settings - You determine these when you create your app ** //
 
-define('APP_NAME', 'take5app');  				/** Your name for your app */
-define('APP_SECRET_KEY', 'beyond comfort');   	/** A password you make up */
+define('APP_NAME', '');  				/** Your name for your app */
+define('APP_SECRET_KEY', '');   	/** A password you make up */
 
 
 // ** MySQL settings - You can get this info from your web host ** //
 
-define('DB_NAME', 'drawonth_take5app');				/** The name of the database */
-define('DB_USER', 'root');				/** MySQL database username */
-define('DB_PASSWORD', 'root');			/** MySQL database password */
-define('DB_HOST', 'localhost');			/** MySQL hostname */
+define('DB_NAME', '');				/** The name of the database */
+define('DB_USER', '');				/** MySQL database username */
+define('DB_PASSWORD', '');			/** MySQL database password */
+define('DB_HOST', '');			/** MySQL hostname */
 
 
 
 // ** fetchable_tables is a list of all tables that the client is allow to query ** //
 
-$fetchable_tables = array( 't5_checklists', 't5_questions' );
+$fetchable_tables = array(  );
 
 
 // ** storeable_tables is a list of all tables that the client is allow to update ** //
 
-$storeable_tables = array( 't5_reviews', 't5_answers' );
+$storeable_tables = array(  );
 
 
 
